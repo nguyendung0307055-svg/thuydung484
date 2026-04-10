0b447a1cb45090b8e81c560a470d071949ba12fa
thuydung <nguyendung0307055@gmail.com>
2026-04-10 09:47:55 +0700

    fix docker

--- Program.cs
+++ Program.cs
@@ -1,5 +1,4 @@
-﻿
-using ConnectDB.Data;
+﻿using ConnectDB.Data;
 using Microsoft.EntityFrameworkCore;
 
 namespace thuydung484
@@ -9,29 +8,26 @@ namespace thuydung484
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddDbContext<AppDbContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-            // Add services to the container.
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+                options.UseSqlServer(
+                    builder.Configuration.GetConnectionString("DefaultConnection")));
 
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+            // ⚠️ BỎ điều kiện Development
+            app.UseSwagger();
+            app.UseSwaggerUI();
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
